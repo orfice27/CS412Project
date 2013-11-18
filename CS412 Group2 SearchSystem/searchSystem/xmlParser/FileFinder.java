48049bc51e46ed427d9381a1cb0a9b575bc41c3d
dvdcxn <david_coxon1@hotmail.com>
2013-11-18 15:52:10 +0000

    Updated XMLParser which can parse an entire folder of files and its sub folders, or a single file. It also writes each parsed file to a .txt file. An arraylist containing links to each can be given by the class.

---
+++ CS412 Group2 SearchSystem/searchSystem/xmlParser/FileFinder.java
@@ -0,0 +1,40 @@
+package xmlParser;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author David Coxon
+ * 
+ * Simple class which takes in two strings, one a file path and the other 
+ * an extension, and checks if 
+ *
+ */
+public class FileFinder {
+	private String ext;
+	private ArrayList<File> filteredFiles;
+	
+	public FileFinder(String path, String ext){
+		this.ext=ext;
+		filteredFiles = new ArrayList<File>();
+		scanForExt(new File(path));
+	}
+	
+	public ArrayList<File> getSelectedFiles(){
+		return filteredFiles;
+	}
+	
+	private void scanForExt(File f){		
+			if (f.isDirectory()){
+				for(File p: f.listFiles()){
+				scanForExt(p);
+			}
+			}
+			else{
+				if(f.getName().endsWith(ext)){
+					filteredFiles.add(f);
+				}			
+			}						
+	}
+}
