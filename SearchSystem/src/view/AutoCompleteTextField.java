177667caab2ad9889514c692f6e3fdf6f09a3b79
dvdcxn <david.coxon.2013@uni.strath.ac.uk>
2014-01-06 17:20:00 +0000

    Update AutoCompleteTextField.java
    Changed autocomplete behaviour so that it only triggers on press of tab key

--- SearchSystem/src/view/AutoCompleteTextField.java
+++ SearchSystem/src/view/AutoCompleteTextField.java
@@ -29,203 +29,198 @@ import javax.swing.event.DocumentListener;
  */
 @SuppressWarnings("serial")
 public class AutoCompleteTextField extends JTextField implements KeyListener,
-            DocumentListener {
-    private ArrayList<String> possibilities;
-    private int currentGuess;
-    private Color incompleteColor;
-    private boolean areGuessing;
-    private boolean caseSensitive;
+DocumentListener {
+	private ArrayList<String> possibilities;
+	private int currentGuess;
+	private Color incompleteColor;
+	private boolean areGuessing;
+	private boolean caseSensitive;
 
-    /**
-     * Constructs a new AutoCompleteTextField with the 5 columns by default and
-     * no case sensitivity on comparisons of guesses to entered text.
-     */
-    public AutoCompleteTextField() {
-        this(5, false);
-    }
+	/**
+	 * Constructs a new AutoCompleteTextField with the 5 columns by default and
+	 * no case sensitivity on comparisons of guesses to entered text.
+	 */
+	public AutoCompleteTextField() {
+		this(5, false);
+	}
 
-    /**
-     * Constructs a new AutoCompleteTextField with the specified number of
-     * columns with no case sensitivity when comparing the current guess to the
-     * text entered.
-     * @param columns The number of columns you wish for the width of this AutoCompleteTextField.
-     */
-    public AutoCompleteTextField(int columns) {
-        this(columns, false);
-    }
+	/**
+	 * Constructs a new AutoCompleteTextField with the specified number of
+	 * columns with no case sensitivity when comparing the current guess to the
+	 * text entered.
+	 * @param columns The number of columns you wish for the width of this AutoCompleteTextField.
+	 */
+	public AutoCompleteTextField(int columns) {
+		this(columns, false);
+	}
 
-    /**
-     * Creates a new AutoCompleteTextField with the given number of columns in
-     * width and the setting for case sensitivity when comparing the current
-     * guess to the entered text.
-     * @param columns The number of columns of text for this component.
-     * @param caseSensitive <code>true</code> or <code>false</code> depending on if you want comparisons to be case sensitive or not.
-     */
-    public AutoCompleteTextField(int columns, boolean caseSensitive) {
-        super.setColumns(columns);
-        this.possibilities = new ArrayList<String>();
-        this.incompleteColor = Color.GRAY.brighter();
-        this.currentGuess = -1;
-        this.areGuessing = false;
-        this.caseSensitive = caseSensitive;
-        this.addKeyListener(this);
-        this.setFocusTraversalKeysEnabled(false);
-        this.getDocument().addDocumentListener(this);
-    }
+	/**
+	 * Creates a new AutoCompleteTextField with the given number of columns in
+	 * width and the setting for case sensitivity when comparing the current
+	 * guess to the entered text.
+	 * @param columns The number of columns of text for this component.
+	 * @param caseSensitive <code>true</code> or <code>false</code> depending on if you want comparisons to be case sensitive or not.
+	 */
+	public AutoCompleteTextField(int columns, boolean caseSensitive) {
+		super.setColumns(columns);
+		this.possibilities = new ArrayList<String>();
+		this.incompleteColor = Color.GRAY.brighter();
+		this.currentGuess = -1;
+		this.areGuessing = false;
+		this.caseSensitive = caseSensitive;
+		this.addKeyListener(this);
+		this.setFocusTraversalKeysEnabled(false);
+		this.getDocument().addDocumentListener(this);
+	}
 
-    /** Add a new possibility to the list of possibilities.
-     * Add a new possibility to the list of possibilities for the
-     * AutoCommpleteTextField to process.
-     * @param possibility The new possibility to add.
-     */
-    public void addPossibility(String possibility) {
-        this.possibilities.add(possibility);
-        Collections.sort(possibilities);
-    }
+	/** Add a new possibility to the list of possibilities.
+	 * Add a new possibility to the list of possibilities for the
+	 * AutoCommpleteTextField to process.
+	 * @param possibility The new possibility to add.
+	 */
+	public void addPossibility(String possibility) {
+		this.possibilities.add(possibility);
+		Collections.sort(possibilities);
+	}
 
-    /** Removes a possibility from the list of possibilities.
-     * Removes the given possibility from the list of possibilities so that it
-     * will no longer be matched.
-     * @param possibility The possibility to remove.
-     */
-    public void removePossibility(String possibility) {
-        this.possibilities.remove(possibility);
-    }
+	/** Removes a possibility from the list of possibilities.
+	 * Removes the given possibility from the list of possibilities so that it
+	 * will no longer be matched.
+	 * @param possibility The possibility to remove.
+	 */
+	public void removePossibility(String possibility) {
+		this.possibilities.remove(possibility);
+	}
 
-    /** Removes all possibilities in the list.
-     * Removes every possibility in the list and starts over with an empty list
-     * ready for new possibilities to be added.
-     */
-    public void removeAllPossibilities() {
-        this.possibilities = new ArrayList<String>();
-    }
+	/** Removes all possibilities in the list.
+	 * Removes every possibility in the list and starts over with an empty list
+	 * ready for new possibilities to be added.
+	 */
+	public void removeAllPossibilities() {
+		this.possibilities = new ArrayList<String>();
+	}
 
-    /** Sets the color to draw the incomplete guess in.
-     * This sets the color that the incomplete guess text is drawn in.
-     * @param incompleteColor The new color to draw the incomplete guess with.
-     */
-    public void setIncompleteColor(Color incompleteColor) {
-        this.incompleteColor = incompleteColor;
-    }
+	/** Sets the color to draw the incomplete guess in.
+	 * This sets the color that the incomplete guess text is drawn in.
+	 * @param incompleteColor The new color to draw the incomplete guess with.
+	 */
+	public void setIncompleteColor(Color incompleteColor) {
+		this.incompleteColor = incompleteColor;
+	}
 
-    /** Returns the current guess from the list of possibilities.
-     * Returns the string at the location of the current guess in the list of 
-     * possibilities.
-     * @return The current guess as a String.
-     */
-    private String getCurrentGuess() {
-        if (this.currentGuess != -1)
-            return this.possibilities.get(this.currentGuess);
+	/** Returns the current guess from the list of possibilities.
+	 * Returns the string at the location of the current guess in the list of 
+	 * possibilities.
+	 * @return The current guess as a String.
+	 */
+	private String getCurrentGuess() {
+		if (this.currentGuess != -1)
+			return this.possibilities.get(this.currentGuess);
 
-        return this.getText();
-    }
+		return this.getText();
+	}
 
-    /**
-     * Changes the current case sensitive setting to the given setting.
-     * @param caseSensitive <code>true</code> or <code>false</code> depending on if you want comparisons to be case sensitive or not.
-     */
-    public void setCaseSensitive(boolean caseSensitive) {
-        this.caseSensitive = caseSensitive;
-    }
+	/**
+	 * Changes the current case sensitive setting to the given setting.
+	 * @param caseSensitive <code>true</code> or <code>false</code> depending on if you want comparisons to be case sensitive or not.
+	 */
+	public void setCaseSensitive(boolean caseSensitive) {
+		this.caseSensitive = caseSensitive;
+	}
 
-    private void findCurrentGuess() {
-        String entered = this.getText();
-        if (!this.caseSensitive)
-            entered = entered.toLowerCase();
+	private void findCurrentGuess() {
+		String entered = this.getText();
+		if (!this.caseSensitive)
+			entered = entered.toLowerCase();
 
-        for (int i = 0; i < this.possibilities.size(); i++) {
-            currentGuess = -1;
+		for (int i = 0; i < this.possibilities.size(); i++) {
+			currentGuess = -1;
 
-            String possibility = this.possibilities.get(i);
-            if (!this.caseSensitive)
-                possibility = possibility.toLowerCase();
-            if (possibility.startsWith(entered)) {
-                this.currentGuess = i;
-                break;
-            }
-        }
-    }
+			String possibility = this.possibilities.get(i);
+			if (!this.caseSensitive)
+				possibility = possibility.toLowerCase();
+			if (possibility.startsWith(entered)) {
+				this.currentGuess = i;
+				break;
+			}
+		}
+	}
 
-    @Override
-    public void setText(String text) {
-        super.setText(text);
-        this.areGuessing = false;
-        this.currentGuess = -1;
-    }
+	@Override
+	public void setText(String text) {
+		super.setText(text);
+		this.areGuessing = false;
+		this.currentGuess = -1;
+	}
 
-    @Override
-    public void paintComponent(Graphics g) {
-        String guess = this.getCurrentGuess();
-        String drawGuess = guess;
+	@Override
+	public void paintComponent(Graphics g) {
+		String guess = this.getCurrentGuess();
+		String drawGuess = guess;
 
-        super.paintComponent(g);
-        String entered = this.getText();
-        Rectangle2D enteredBounds = g.getFontMetrics().getStringBounds(entered, g);
+		super.paintComponent(g);
+		String entered = this.getText();
+		Rectangle2D enteredBounds = g.getFontMetrics().getStringBounds(entered, g);
 
-        if (!(this.caseSensitive)) {
-            entered = entered.toLowerCase();
-            guess = guess.toLowerCase();
-        }
-        
-        if (!(guess.startsWith(entered)))
-            this.areGuessing = false;
+		if (!(this.caseSensitive)) {
+			entered = entered.toLowerCase();
+			guess = guess.toLowerCase();
+		}
 
-        if (entered != null && !(entered.equals("")) 
-                && this.areGuessing) {
-            String subGuess = drawGuess.substring(entered.length(), drawGuess.length());
-            Rectangle2D subGuessBounds = g.getFontMetrics().getStringBounds(drawGuess, g);
+		if (!(guess.startsWith(entered)))
+			this.areGuessing = false;
 
-            int centeredY = ((getHeight() / 2) + (int)(subGuessBounds.getHeight() / 2));
+		if (entered != null && !(entered.equals("")) 
+				&& this.areGuessing) {
+			String subGuess = drawGuess.substring(entered.length(), drawGuess.length());
+			Rectangle2D subGuessBounds = g.getFontMetrics().getStringBounds(drawGuess, g);
 
-            g.setColor(this.incompleteColor);
-            g.drawString(subGuess + "   press ENTER to send or \u2192 to fill", (int)(enteredBounds.getWidth()) + 2, centeredY - 2);
-        }
-    }
+			int centeredY = ((getHeight() / 2) + (int)(subGuessBounds.getHeight() / 2));
 
-    public void keyTyped(KeyEvent e) { }
+			g.setColor(this.incompleteColor);
+			g.drawString(subGuess + "   press ENTER to send or \u2192 to fill", (int)(enteredBounds.getWidth()) + 2, centeredY - 2);
+		}
+	}
 
-    public void keyPressed(KeyEvent e) { 
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-            if (this.areGuessing) {
-                this.setText(this.getCurrentGuess());
-                this.areGuessing = false;
-            }
-        }
+	public void keyTyped(KeyEvent e) { }
 
-        if (e.getKeyCode() == KeyEvent.VK_TAB) {
-            if (this.areGuessing) {
-                this.setText(this.getCurrentGuess());
-                this.areGuessing = false;
-                e.consume();
-            }
-        }
-    }
-    
-    public void keyReleased(KeyEvent e) { }
+	public void keyPressed(KeyEvent e) { 
+		if (e.getKeyCode() == KeyEvent.VK_TAB) {
+			if (this.areGuessing) {
+				this.setText(this.getCurrentGuess());
+				this.areGuessing = false;
+				e.consume();
 
-    public void insertUpdate(DocumentEvent e) {
-        String temp = this.getText();
+			}
+		}
+	}
 
-        if (temp.length() == 1)
-            this.areGuessing = true;
 
-        if (this.areGuessing)
-            this.findCurrentGuess();
+	public void keyReleased(KeyEvent e) { }
 
-    }
+	public void insertUpdate(DocumentEvent e) {
+		String temp = this.getText();
 
-    public void removeUpdate(DocumentEvent e) {
-        String temp = this.getText();
+		if (temp.length() == 1)
+			this.areGuessing = true;
 
-        if (!(this.areGuessing))
-            this.areGuessing = true;
+		if (this.areGuessing)
+			this.findCurrentGuess();
 
-        if (temp.length() == 0)
-            this.areGuessing = false;
-        else if (this.areGuessing) {
-            this.findCurrentGuess();
-        }
-    }
+	}
 
-    public void changedUpdate(DocumentEvent e) { }
+	public void removeUpdate(DocumentEvent e) {
+		String temp = this.getText();
+
+		if (!(this.areGuessing))
+			this.areGuessing = true;
+
+		if (temp.length() == 0)
+			this.areGuessing = false;
+		else if (this.areGuessing) {
+			this.findCurrentGuess();
+		}
+	}
+
+	public void changedUpdate(DocumentEvent e) { }
 }
