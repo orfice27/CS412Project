c19325ae3533342f3325ed550f29577ba97858ff
Tom Curran <tom@tomcurran.org>
2014-01-07 11:53:58 +0000

    history tab looks better

--- SearchSystem/src/view/HistoryTab.java
+++ SearchSystem/src/view/HistoryTab.java
@@ -1,11 +1,17 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Insets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 /**
  * Tab for displaying query history
@@ -13,11 +19,17 @@ import javax.swing.JTextArea;
 public class HistoryTab extends JScrollPane {
 
 	private static final long serialVersionUID = 740032827501268132L;
+	private static final String TEMPLATE_HISTORY_ENTRY = "<html><span style='color: rgb(102, 102, 102); font-size: 13px;'>%s</span> - <span style='color: rgb(68, 68, 68); font-size: 13px;'>%s</span></html>";
+	private static final String DATE_FORMAT = "EEEE, MMMMM dd, yyyy hh:mm a";
 
 	private JPanel container;
-	private JTextArea textArea;
+	private List<JButton> historyEntryViews;
+	private SimpleDateFormat dateFormat;
 
 	public HistoryTab() {
+		historyEntryViews = new ArrayList<JButton>();
+		dateFormat = new SimpleDateFormat(DATE_FORMAT);
+
 		container = new JPanel();
 		container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
 
@@ -25,21 +37,24 @@ public class HistoryTab extends JScrollPane {
 		setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-		createTextView();
-	}
-
-	private void createTextView() {
-		textArea = new JTextArea();
-		textArea.setEditable(false);
-		container.add(textArea);
 	}
 
 	public void appendHistoryEntry(String query, Date time) {
-		textArea.append(String.format("%s @ %s%n", query, time));
+		JButton historyEntryView = new JButton();
+		historyEntryView.setMargin(new Insets(0, 0, 0, 0));
+		historyEntryView.setHorizontalAlignment(SwingConstants.LEFT);
+		historyEntryView.setBorderPainted(false);
+		historyEntryView.setOpaque(false);
+		historyEntryView.setBackground(Color.WHITE);
+		historyEntryView.setText(String.format(TEMPLATE_HISTORY_ENTRY, dateFormat.format(time), query));
+		historyEntryViews.add(historyEntryView);
+		container.add(historyEntryView);
 	}
 
 	public void clearHistory() {
-		textArea.setText("");
+		for (JButton historyEntry : historyEntryViews) {
+			container.remove(historyEntry);
+		}
 	}
 
 }
